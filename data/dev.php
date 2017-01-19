46cdf0c0eb61fd5e1e4eea378887457bd1b3aef8
jry <598821125@qq.com>
2017-01-19 12:45:38 +0800

    修复css加载失败

--- data/dev.php
+++ data/dev.php
@@ -17,7 +17,7 @@
 //$_SERVER[ENV_PRE.'DEV_MODE']  = 'true';
 //$_SERVER[ENV_PRE.'DB_TYPE']   = 'mysql';
 //$_SERVER[ENV_PRE.'DB_HOST']   = '127.0.0.1';
-//$_SERVER[ENV_PRE.'DB_NAME']   = 'opencmf';
+//$_SERVER[ENV_PRE.'DB_NAME']   = 'lingyun';
 //$_SERVER[ENV_PRE.'DB_USER']   = 'root';
 //$_SERVER[ENV_PRE.'DB_PWD']    = '';
 //$_SERVER[ENV_PRE.'DB_PORT']   = '3306';
