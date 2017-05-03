c294f6f010f7fa5e4594f38878b59ce0d98f210a
xieqingqing <xieqingqing@kuaxue.com>
2017-05-03 17:12:33 +0800

    xie updtate tp 5.0.8

--- thinkphp/library/think/db/connector/Mysql.php
+++ thinkphp/library/think/db/connector/Mysql.php
@@ -21,6 +21,8 @@ use think\Log;
 class Mysql extends Connection
 {
 
+    protected $builder = '\\think\\db\\builder\\Mysql';
+
     /**
      * 解析pdo连接的dsn信息
      * @access protected
@@ -127,4 +129,5 @@ class Mysql extends Connection
     {
         return true;
     }
+
 }
